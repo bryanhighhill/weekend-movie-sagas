a87d40c8ce4ceef08287b1334446f62afa725d1d
Bryan Highhill <bryanhighhill@gmail.com>
2023-02-16 16:15:26 -0600

    sucessfully created movieForm that is being consumed in Carousel. MovieForm is utilizing react form element to take in user-generated input values and select for genre. The info is then sent as a payload to a post request which is successfully posting to the db and making a get request to update the movie carousel.

---
+++ src/components/MovieForm/MovieForm.jsx
@@ -0,0 +1,145 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import './MovieForm.css';
+
+const MovieForm = () => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [poster, setPoster] = useState('');
+    const [genre, setGenre] = useState(0);
+    const [formVisible, setFormVisible] = useState(false);
+    const dispatch = useDispatch();
+    
+    const isInvalid = (title, poster, description) => {
+        if (title.length <= 0 && description.length <= 0 && poster.length <=0) {
+            alert('inputs cannot be blank')
+            return true;
+        }
+        if (title.length <= 0 || description.length <= 0 || poster.length <=0) {
+            alert('inputs cannot be blank')
+            return true;
+        }
+        if (description.length <= 0 && poster.length <=0) {
+            alert('inputs cannot be blank')
+            return true;
+        }
+        if (title.length <= 0 && poster.length <=0) {
+            alert('inputs cannot be blank')
+            return true;
+        }
+        if (title.length <= 0) {
+            alert('must enter a movie title')
+            return true;
+        }
+        if (poster.length <= 0){
+            alert('must enter a poster')
+            return true;
+        }
+        if (description.length <= 0){
+            alert('must enter a description')
+            return true;
+        }
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const genre_id = Number(genre);
+        const newMovie = {
+            title,
+            poster,
+            description,
+            genre_id
+        }
+        if (!isInvalid(title, poster, description)) {
+            dispatch({
+                type: 'POST_MOVIE', 
+                payload: newMovie
+            });
+        setTitle('');
+        setPoster('');  
+        setDescription('');      
+        }
+    }
+
+    return (
+        <>
+            <button 
+                className="form-visible-button"
+                onClick={() => {setFormVisible(!formVisible)}}
+            >
+                Add Movie Form
+            </button>
+            {formVisible && <div className="movie-form">
+                <br />
+                {/* dispatch to POST function called here */}
+                <form onSubmit={onSubmit}>
+
+                    {/* form needs 3 inputs, 1 dropdown: title, poster, description. Dropdown with genre options */}
+                    {/* collect movie title here */}
+                    <label htmlFor="title">Title: </label>
+                    <input 
+                        id="title" 
+                        name="title"
+                        value={title} 
+                        placeholder="movie title"
+                        onChange={(event) => setTitle(event.target.value)}
+                    />
+                    &nbsp; &nbsp; &nbsp; &nbsp; 
+
+                    {/* collect poster link here */}
+                    <label htmlFor="poster">Poster link: </label>
+                    <input 
+                        id="poster" 
+                        //name attribute should match html attribute
+                        name="poster"
+                        value={poster} 
+                        placeholder="poster link"
+                        onChange={(event) => setPoster(event.target.value)}
+                    /> 
+                    &nbsp; &nbsp; &nbsp; &nbsp; 
+
+                    {/* collect movie description here */}
+                    <label htmlFor="description">Movie description: </label>
+                    <input 
+                        id="description" 
+                        //name attribute should match html attribute
+                        name="description"
+                        value={description} 
+                        placeholder="description link"
+                        onChange={(event) => setDescription(event.target.value)}
+                    /> 
+                    &nbsp; &nbsp; &nbsp; &nbsp;
+
+                    {/* genre dropdown here */}
+                    <label>
+                        Select a movie genre:
+                        <select onChange={(event) => setGenre(event.target.value)}>
+                            <option value="1">Adventure</option>
+                            <option value="2">Animated</option>
+                            <option value="3">Biographical</option>
+                            <option value="4">Comedy</option>
+                            <option value="5">Disaster</option>
+                            <option value="6">Drama</option>
+                            <option value="7">Epic</option>
+                            <option value="8">Fantasy</option>
+                            <option value="9">Musical</option>
+                            <option value="10">Romantic</option>
+                            <option value="11">Science Fiction</option>
+                            <option value="12">Space-Opera</option>
+                            <option value="13">Superhero</option>
+                        </select>
+                        </label>
+
+                    {/* add submit button here */}
+                    <button 
+                        type="submit"
+                        className="submit-movie-button"
+                    >
+                        Submit Movie</button>
+                </form>
+            </div>}
+        </>
+    )
+}
+
+export default MovieForm;
